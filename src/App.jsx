d51c0ef222734cfd33e3acfeab7ea4ab8c913b92
mIBRAHIM707 <ibrahimclash707@gmail.com>
2025-02-22 16:30:06 +0500

    async search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,85 @@
-// App.jsx
-import './App.css'
+import "./App.css";
 import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
-useEffect(() => {
-  let authParams = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body:
-      "grant_type=client_credentials&client_id=" +
-      clientId +
-      "&client_secret=" +
-      clientSecret,
-  };
-
-  fetch("https://accounts.spotify.com/api/token", authParams)
-    .then((result) => result.json())
-    .then((data) => {
-      setAccessToken(data.access_token);
-    });
-}, []);
-
 function App() {
   const [searchInput, setSearchInput] = useState("");
   const [accessToken, setAccessToken] = useState("");
 
+  useEffect(() => {
+    let authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        clientId +
+        "&client_secret=" +
+        clientSecret,
+    };
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then((result) => result.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      });
+  }, []);
+
+  async function search() {
+    let artistParams = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    // Get Artist
+    const artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+    console.log("Search Input: " + searchInput);
+    console.log("Artist ID: " + artistID);  
+  }
+
   return (
-    <Container>
-    <InputGroup style={{ display: 'flex', alignItems: 'center' }}>
-      <FormControl
-        placeholder="Search For Artist"
-        type="input"
-        aria-label="Search for an Artist"
-        onKeyDown={() => {}} // search function
-        onChange={() => {}} // setSearch
-        style={{
-          width: "300px",
-          height: "38px",
-          borderWidth: "0px",
-          borderStyle: "solid",
-          borderRadius: "5px",
-          marginRight: "10px",
-          paddingLeft: "10px",
-        }}
-      />
-  
-      <Button style={{height: "42px", padding: "0px 24px", fontSize: "17px"}} onClick={() => {}}>Search</Button>
-    </InputGroup>
-  </Container>
-  )
+    <>
+      <Container>
+        <InputGroup>
+          <FormControl
+            placeholder="Search For Artist"
+            type="input"
+            aria-label="Search for an Artist"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+            style={{
+              width: "300px",
+              height: "35px",
+              borderWidth: "0px",
+              borderStyle: "solid",
+              borderRadius: "5px",
+              marginRight: "10px",
+              paddingLeft: "10px",
+            }}
+          />
+
+          <Button onClick={search}>Search</Button>
+        </InputGroup>
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
