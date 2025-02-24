3a071465e6fbe376dc43bb80e1f43b7c96f1bcdc
mIBRAHIM707 <ibrahimclash707@gmail.com>
2025-02-24 21:56:53 +0500

    improving

---
+++ src/components/AlbumCard.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const AlbumCard = ({ album }) => {
+  return (
+    <Card
+      style={{
+        backgroundColor: "white",
+        borderRadius: "5px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        minWidth: "200px",
+      }}
+    >
+      <Link to={`/album/${album.id}`}>
+        <Card.Img
+          variant="top"
+          src={album.images[0].url}
+          style={{
+            borderRadius: "4%",
+            width: "100%",
+            objectFit: "cover",
+          }}
+        />
+      </Link>
+      <Card.Body style={{ textAlign: "center" }}>
+        <Card.Title
+          style={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            fontWeight: "bold",
+            fontSize: "18px",
+            marginTop: "10px",
+            color: "black",
+            maxWidth: "180px",
+          }}
+        >
+          {album.name}
+        </Card.Title>
+        <Card.Text style={{ color: "black" }}>
+          Release Date: <br /> {album.release_date}
+        </Card.Text>
+        <Button
+          href={album.external_urls.spotify}
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            fontWeight: "bold",
+            fontSize: "15px",
+            borderRadius: "5px",
+            padding: "10px",
+          }}
+        >
+          Album Link
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+AlbumCard.propTypes = {
+  album: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(
+      PropTypes.shape({
+        url: PropTypes.string.isRequired,
+      })
+    ).isRequired,
+    release_date: PropTypes.string.isRequired,
+    external_urls: PropTypes.shape({
+      spotify: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default AlbumCard;
---
+++ src/services/spotifyApi.js
@@ -0,0 +1,62 @@
+const BASE_URL = 'https://api.spotify.com/v1';
+
+class SpotifyService {
+  constructor() {
+    this.accessToken = localStorage.getItem('accessToken');
+  }
+
+  setAccessToken(token) {
+    this.accessToken = token;
+    localStorage.setItem('accessToken', token);
+  }
+
+  getHeaders() {
+    return {
+      'Authorization': `Bearer ${this.accessToken}`,
+      'Content-Type': 'application/json',
+    };
+  }
+
+  async searchArtist(searchInput) {
+    const response = await fetch(
+      `${BASE_URL}/search?q=${searchInput}&type=artist`,
+      { headers: this.getHeaders() }
+    );
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.error?.message || 'Failed to search artist');
+    return data;
+  }
+
+  async getArtistAlbums(artistId, page = 1, limit = 20) {
+    const offset = (page - 1) * limit;
+    const response = await fetch(
+      `${BASE_URL}/artists/${artistId}/albums?include_groups=album&market=US&limit=${limit}&offset=${offset}`,
+      { headers: this.getHeaders() }
+    );
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.error?.message || 'Failed to fetch albums');
+    return data;
+  }
+
+  async getAlbumDetails(albumId) {
+    const response = await fetch(
+      `${BASE_URL}/albums/${albumId}`,
+      { headers: this.getHeaders() }
+    );
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.error?.message || 'Failed to fetch album details');
+    return data;
+  }
+
+  async getAlbumTracks(albumId) {
+    const response = await fetch(
+      `${BASE_URL}/albums/${albumId}/tracks`,
+      { headers: this.getHeaders() }
+    );
+    const data = await response.json();
+    if (!response.ok) throw new Error(data.error?.message || 'Failed to fetch album tracks');
+    return data;
+  }
+}
+
+export default new SpotifyService();
